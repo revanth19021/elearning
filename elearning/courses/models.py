4a889dd995363b6f03214eb3c78d6656bb9775d6
revanth19021 <revanth200319@gmail.com>
2026-04-15 10:25:20 +0530

    Committed

--- elearning/courses/models.py
+++ elearning/courses/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Course(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    image = models.URLField(blank=True, null=True)   
 
     def __str__(self):
         return self.title
--- elearning/courses/views.py
+++ elearning/courses/views.py
@@ -5,9 +5,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Course, Video
 
 
+from .models import Course
+
 def course_list(request):
     courses = Course.objects.all()
-    return render(request, 'courses/course_list.html', {'courses': courses})
+    return render(request, 'course_list.html', {'courses': courses})
+
 
 def course_detail(request, course_id):
     course = get_object_or_404(Course, id=course_id)
